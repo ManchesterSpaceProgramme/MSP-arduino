89ba403f93aae16242fa4f7b3bbc48a2b89d0f4a
harvy2004 <harvinder@harvinderatwal.com>
2014-09-28 18:31:45 +0100

    Pete's payload added
    Added pins for pete's payload and camera pin, this is set to turn on for
    20 seconds after rtty string is sent.
    Renamed test_rtty_gps

---
+++ doge1_rtty_gps_new_payload/doge1_rtty_gps_new_payload.ino
@@ -0,0 +1,149 @@
+//Title:    STRATODEAN Tracker code
+//Author:   Mark Ireland
+//Website:  http://www.stratodean.co.uk
+//Notes:    Thanks goes to all the existing UKHAS enthusiasts who helped with providing example code for others to use.
+//          Thanks also to the guys on the UKHAS #highaltitude irc channel for their invaluable help.
+
+//Github test
+//We need SoftwareSerial port as we are using the hardware port for GPS
+#include <SoftwareSerial.h>
+//This is a modifed version of the TinyGPS libaray for UBlox GPS chips
+#include <TinyGPS_UBX.h>
+//Include our separate code for ease of reading
+#include "rtty.h"
+#include "gps.h"
+
+#define ENABLE_RADIO 8
+#define GPSTX 4
+#define GPSRX 3
+#define NTX2 9
+#define CAMERA 2
+#define VOLTAGE 17
+
+//Character buffer for transmission
+#define DATASIZE 256
+char data[DATASIZE];
+//s_id as sentence id to have a unique number for each transmission
+uint16_t s_id = 0;
+//initialise our classes
+RTTY rtty(NTX2);
+GPS gps(GPSRX, GPSTX);
+
+//*************************************************************************************
+//
+//setup()
+//
+//*************************************************************************************
+
+void setup() {
+  //Initialise GPS
+  gps.start();
+  setPwmFrequency(NTX2, 1);
+  pinMode(ENABLE_RADIO, OUTPUT);
+  pinMode(CAMERA, OUTPUT);
+  pinMode(VOLTAGE, INPUT);
+  digitalWrite(ENABLE_RADIO, HIGH);
+  Serial.println(F("GPS initialised"));  
+}
+
+//*************************************************************************************
+//
+//loop()
+//
+//*************************************************************************************
+
+void loop() {
+  //Call gps.get_info and, along with the s_id and battery, put it altogether into the string called 'data'
+  snprintf(data, DATASIZE, "$$DOGE1,%d,%s", s_id, gps.get_info(), getVoltage());
+  //print this to the screen and the ram
+  //Serial.println(data);
+
+  rtty.send(data);
+  //increment the id next time.
+  s_id++;
+  //delay(500);
+  
+  digitalWrite(CAMERA, HIGH);
+  delay(20000);
+  digitalWrite(CAMERA, LOW);
+}
+
+//*************************************************************************************
+//
+//helper routines()
+//
+//*************************************************************************************
+
+
+//subroutine to give the amount of ram available on the board
+int freeRam () {
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
+
+
+void setPwmFrequency(int pin, int divisor) {
+ byte mode;
+ if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+ switch(divisor) {
+ case 1:
+ mode = 0x01;
+ break;
+ case 8:
+ mode = 0x02;
+ break;
+ case 64:
+ mode = 0x03;
+ break;
+ case 256:
+ mode = 0x04;
+ break;
+ case 1024:
+ mode = 0x05;
+ break;
+ default:
+ return;
+ }
+ if(pin == 5 || pin == 6) {
+ TCCR0B = TCCR0B & 0b11111000 | mode;
+ }
+ else {
+ TCCR1B = TCCR1B & 0b11111000 | mode;
+ }
+ }
+ else if(pin == 3 || pin == 11) {
+ switch(divisor) {
+ case 1:
+ mode = 0x01;
+ break;
+ case 8:
+ mode = 0x02;
+ break;
+ case 32:
+ mode = 0x03;
+ break;
+ case 64:
+ mode = 0x04;
+ break;
+ case 128:
+ mode = 0x05;
+ break;
+ case 256:
+ mode = 0x06;
+ break;
+ case 1024:
+ mode = 0x7;
+ break;
+ default:
+ return;
+ }
+ TCCR2B = TCCR2B & 0b11111000 | mode;
+ }
+ 
+ float getVoltage(){
+   float voltage;
+  //Obtain RAW voltage data
+  voltage = analogRead(VOLTAGE);
+ }  
+}
---
+++ doge1_rtty_gps_new_payload/rtty.cpp
@@ -0,0 +1,85 @@
+#include "rtty.h"
+
+uint8_t _pin = 0;
+
+//Setting our pin assignments
+RTTY::RTTY(uint8_t pin) {
+  pinMode(pin, OUTPUT);
+  _pin = pin;
+}
+
+//Method to send data string
+void RTTY::send(char *data) {
+
+  char c;
+  char chksum_str[6];
+
+  //Call checksum routine
+  unsigned int CHECKSUM = crc16_chksum(data);
+  sprintf(chksum_str, "*%04X\n", CHECKSUM);
+  //Concatinate data with checksum
+  strcat(data, chksum_str);
+
+  c = *data++;
+  //While we haven't reached the end of the data string, keep sending bytes
+  while (c != '\0') {
+    send_byte(c);
+    c = *data++;
+  }
+
+}
+void RTTY::send_byte(char c) {
+  uint8_t i;
+
+  send_bit(0); // Start bit
+
+  // Send bits for for char LSB first	
+  for (i=0; i<7; i++) { // Change this here 7 or 8 for ASCII-7 / ASCII-8
+    if (c & 1)
+      send_bit(1);
+    else
+      send_bit(0);	
+    c = c >> 1;
+  }
+
+  send_bit(1); // Stop bit
+  send_bit(1); // Stop bit
+}
+
+void RTTY::send_bit(uint8_t bit) {
+  if (bit) {
+    // high
+    analogWrite(_pin, 110);
+  } 
+  else {
+    // low
+    analogWrite(_pin, 100);
+  }
+
+  //delayMicroseconds(3333); // 300 baud
+  //delayMicroseconds(13325); // 75 baud
+  delayMicroseconds(10000); // base delay for 45/50
+  delayMicroseconds(10000); // 50 baud
+  //delayMicroseconds(12222); // 45 baud
+}
+
+//Checksum routine
+uint16_t RTTY::crc16_chksum(char *str) 
+{
+  size_t i;
+  uint16_t crc;
+  uint8_t c;
+
+  crc = 0xFFFF;
+
+  // Calculate checksum ignoring the first two $s
+  for (i = 2; i < strlen(str); i++) 
+  {
+    c = str[i];
+    crc = _crc_xmodem_update (crc, c);
+  }
+  return crc;
+}
+
+
+
