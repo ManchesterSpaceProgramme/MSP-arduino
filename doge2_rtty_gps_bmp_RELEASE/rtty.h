a79cfaf9c5f5de3c8cfc474fc5480d9208ac1727
harvy2004 <harvinder@harvinderatwal.com>
2015-06-06 16:00:16 +0100

    Added Doge2 code
    Only changed callsign and debug messages to doge2. All hardware is the
    same.

---
+++ doge2_rtty_gps_bmp_RELEASE/rtty.h
@@ -0,0 +1,21 @@
+#ifndef RTTY_H
+#define RTTY_H
+
+#include <Arduino.h>
+#include <string.h>
+#include <util/crc16.h>
+
+//class definition
+
+class RTTY {  
+public:
+  RTTY(uint8_t pin);
+  void send(char *data);
+  void send_byte(char c);
+  void send_bit(uint8_t bit);
+  uint16_t crc16_chksum(char *str);
+};
+
+#endif
+
+
